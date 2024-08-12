ac016714c4ffe048443ca97f1801f127ed916dcc
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-12 21:38:18 +0300

    section play

--- src/js/components/js_item.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-    const item1 = document.querySelector('.js_item_1');
-    const item2 = document.querySelector('.js_item_2');
-    const item3 = document.querySelector('.js_item_3');
-
-    item1.onclick = function () {};
-}
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 import js_burger from './components/js_burger.js';
-// import js_item from './components/js_item.js';
 
 (function () {
     js_burger();
-    // js_item();
 })();
