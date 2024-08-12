e6c0f06fce1ca3e8b536dec57b62b814ce3abacf
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-12 14:28:30 +0300

    section promo

---
+++ src/js/components/js_item.js
@@ -0,0 +1,7 @@
+export default function () {
+    const item1 = document.querySelector('.js_item_1');
+    const item2 = document.querySelector('.js_item_2');
+    const item3 = document.querySelector('.js_item_3');
+
+    item1.onclick = function () {};
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 import js_burger from './components/js_burger.js';
+// import js_item from './components/js_item.js';
 
 (function () {
     js_burger();
+    // js_item();
 })();
